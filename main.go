1040ee9bc22e10db8774666e9b1d389994cc2f70
Yu-Heng Lin <yuhenglin0204@gmail.com>
2023-06-24 21:26:59 -0700

    added encode and decode

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
 	"os"
 )
@@ -11,7 +13,36 @@ func Init() {
 	os.Mkdir(GIT_DIR, 0755)
 }
 
+func hashObject(data string) (string, error) {
+	dataBytes := []byte(data)
+	hash := sha1.New()
+	hash.Write(dataBytes)
+	oid := hex.EncodeToString(hash.Sum(nil))
+
+	return oid, nil
+}
+
 func main() {
 	Init()
-	fmt.Println("Hello")
+	// Example usage
+	data := "Hello, World!"
+	oid, err := hashObject(data)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("Object ID:", oid)
+	decodedString := string(data)
+	fmt.Println("Decoded content:", decodedString)
+
+	data = "Goodbye, World!"
+	oid, err = hashObject(data)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println("Object ID:", oid)
+	decodedString = string(data)
+	fmt.Println("Decoded content:", decodedString)
 }
