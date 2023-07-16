af2e1075ac0ed0dfc3ae5d7e6233585808a2bb25
Yu-Heng Lin <yuhenglin0204@gmail.com>
2023-07-15 20:28:07 -0700

    need to do branch and commit later

--- internal/manager.go
+++ internal/manager.go
@@ -7,7 +7,8 @@ import (
 )
 
 type LVCSManager struct {
-	lvcsMan map[string]interface{}
+	lvcsPath string
+	lvcsMan  map[string]interface{}
 }
 
 // NewLVCSManager creates a new LVCSAdd instance
@@ -24,32 +25,77 @@ func NewLVCSManager(lvcsPath string) *LVCSManager {
 	manager["fileHashIO"] = fileHashIOMan
 	manager["init"] = initMan
 	return &LVCSManager{
-		lvcsMan: manager,
+		lvcsPath: lvcsPath,
+		lvcsMan:  manager,
 	}
 }
 
-func (lvcsManager *LVCSManager) Execute(command string, subcommands []string) error {
+func (lvcsManager *LVCSManager) Execute(command string, subcommands []string) (string, error) {
 	switch command {
 	case "hash-object":
-		// TBD
+		// return oid if no error
+		fileHashIOMan, ok := lvcsManager.lvcsMan["fileHashIO"].(*utils.LVCSFileHashIOManager)
+		if !ok {
+			return "", errors.New("Failed to execute hash-object")
+		}
+		if len(subcommands) != 1 {
+			return "", errors.New("number of argumment not correct, expected a path to a file but not found")
+		}
+
+		oid, err := fileHashIOMan.HashObject(subcommands[0])
+		if err != nil {
+			return "", err
+		}
+		return oid, nil
 	case "cat-file":
-		// TBD
+		// return file content if no error
+		fileHashIOMan, ok := lvcsManager.lvcsMan["fileHashIO"].(*utils.LVCSFileHashIOManager)
+		if !ok {
+			return "", errors.New("Failed to execute cat-file")
+		}
+		if len(subcommands) != 1 {
+			return "", errors.New("number of argumment not correct, expected an oid but not found")
+		}
+		content, err := fileHashIOMan.CatFile(subcommands[0])
+		if err != nil {
+			return "", err
+		}
+		return content, nil
 	case "add":
-		// TBD
+		// return success messaage if no error
+		addMan, ok := lvcsManager.lvcsMan["add"].(*utils.LVCSAddManager) // change the receiver later
+		if !ok {
+			return "", errors.New("Failed to execute add")
+		}
+		if len(subcommands) < 1 {
+			return "", errors.New("number of argumment not correct, expected at least one path input but not found")
+		}
+		for _, file := range subcommands {
+			err := addMan.Add(file)
+			if err != nil {
+				return "", errors.New("failed to add:" + file)
+			}
+		}
+		return string("added all of the files success"), nil
 	case "init":
+		// return success message if no error
 		initMan, ok := lvcsManager.lvcsMan["init"].(*utils.LVCSInitManager)
 		if !ok {
-			return errors.New("Failed to execute init")
+			return "", errors.New("Failed to execute init")
 		}
 		err := initMan.Init()
 		if err != nil {
-			return err
+			return "", err
 		}
+		return string("Init .lvcs directory at:" + lvcsManager.lvcsPath + " success"), nil
 	case "commit":
-
+		// TBD
+		return "", nil
+	case "branch":
+		// TBD
+		return "", nil
 	default:
 		// TBD
+		return "", nil
 	}
-
-	return nil
 }
--- internal/manager_test.go
+++ internal/manager_test.go
@@ -16,15 +16,32 @@ func removeLVCSTestDir() error {
 	}
 	return nil
 }
+
 func TestLVCSManager(t *testing.T) {
 	err := removeLVCSTestDir()
 	if err != nil {
-		t.Errorf("failed to delete existing dir")
+		t.Errorf("failed to delete existing dir: %s", err.Error()) // Convert error to string
 	}
 
 	lvcsMan := internal.NewLVCSManager(lvcsManTestDir)
-	err = lvcsMan.Execute("init", []string{})
+	_, err = lvcsMan.Execute("init", []string{})
 	if err != nil {
-		t.Errorf("create .lvcs_test DIR failed")
+		t.Errorf("create .lvcs_test DIR failed: %s", err.Error()) // Convert error to string
+	}
+
+	oid, err := lvcsMan.Execute("hash-object", []string{"../test_data/a.txt"})
+	if err != nil || oid != "84e3a8e13916a5e48349e49fe16cfab6a384b4a9" {
+		t.Errorf("hash-object failed: %s", err.Error()) // Convert error to string
+	}
+
+	content, err := lvcsMan.Execute("cat-file", []string{"84e3a8e13916a5e48349e49fe16cfab6a384b4a9"})
+	expectedContent := "To implement functionality similar to cat-file in Git, where you convert an object ID (OID) back to its corresponding string content"
+	if err != nil || content != expectedContent {
+		t.Errorf("cat-file failed: %s", err.Error()) // Convert error to string
+	}
+
+	_, err = lvcsMan.Execute("add", []string{"../test_data/a.txt", "../test_data/b.txt", "../test_data/ok/abc.txt"})
+	if err != nil {
+		t.Errorf("add failed: %s", err.Error()) // Convert error to string
 	}
 }
